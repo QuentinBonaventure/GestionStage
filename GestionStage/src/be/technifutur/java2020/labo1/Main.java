dfa0e0b82c40430ea2cc7e5bc55a3a4ce4b09a3f
Quentin Bona <bonaventurequentin@gmail.com>
2020-11-05 13:42:43 +0100

    suppression package stage inutil

--- GestionStage/src/be/technifutur/java2020/labo1/Main.java
+++ GestionStage/src/be/technifutur/java2020/labo1/Main.java
@@ -1,8 +1,8 @@
 package be.technifutur.java2020.labo1;
 
-import be.technifutur.java2020.labo1.stages.StageControler;
-import be.technifutur.java2020.labo1.stages.ListStage;
-import be.technifutur.java2020.labo1.stages.StageVue;
+import be.technifutur.java2020.labo1.StageControler;
+import be.technifutur.java2020.labo1.ListStage;
+import be.technifutur.java2020.labo1.StageVue;
 
 public class Main {
     public static void main(String[] args) {
