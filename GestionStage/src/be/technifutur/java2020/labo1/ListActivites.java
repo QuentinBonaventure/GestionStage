f8f80b5451bef35c6faf829967eb33dff74f5188
Quentin Bona <bonaventurequentin@gmail.com>
2020-11-06 16:57:25 +0100

    Ajout listactivité avec hashmap et meme principe que pour stage et activités

---
+++ GestionStage/src/be/technifutur/java2020/labo1/ListActivites.java
@@ -0,0 +1,8 @@
+package be.technifutur.java2020.labo1;
+
+import java.util.HashMap;
+
+public class ListActivites {
+    public static  HashMap<Integer , Activite> maListeActivtés = new HashMap<>();
+
+}
