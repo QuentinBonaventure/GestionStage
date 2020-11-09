586577b7310e3c7be33d7fa91e913901b5ec79a9
Quentin Bona <bonaventurequentin@gmail.com>
2020-11-09 08:37:52 +0100

    list activité

--- GestionStage/src/be/technifutur/java2020/labo1/ListActivites.java
+++ GestionStage/src/be/technifutur/java2020/labo1/ListActivites.java
@@ -4,5 +4,7 @@ import java.util.HashMap;
 
 public class ListActivites {
     public static  HashMap<Integer , Activite> maListeActivtés = new HashMap<>();
-
+// principe meme que stage . sauf qu'il faudra croisé les deux pour pouvoir ajouté une activité la mettre dans sa liste mais a
+  //  la mettre dans l'activité du stage'.
+    // créer new activite dans stage
 }
